e815602a6be541089ecfd2e9b209e981421b2d7c
aloycwl <aloycwl@gmail.com>
2024-01-10 11:10:35 +0800

    Updated import

--- ERC1822/UUPS_Example.sol
+++ ERC1822/UUPS_Example.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: None
 pragma solidity 0.8.0;
 
-import "https://github.com/aloycwl/Proxy.sol/blob/main/ERC1822/UUPS.sol";
+import {UUPSUpgradeable} from "./UUPS.sol"; 
 
 contract NFTExample is UUPSUpgradeable {
 

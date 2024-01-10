0ec2674436b9aa8235953df46d8116a09b1509ca
aloycwl <aloycwl@gmail.com>
2024-01-10 10:54:28 +0800

    Changed folder

---
+++ ERC1822/README.md
@@ -0,0 +1,10 @@
+# ERC1822
+
+Contains:
+
+UUPS.sol - This is a universal upgradeable proxy, meaning it can be upgraded upon implementation.
+Read more at https://www.linkedin.com/pulse/simplest-way-use-erc-1822-universal-upgradeable-proxy-aloysius-chan/
+and https://eips.ethereum.org/EIPS/eip-1822.
+
+
+UUPS_Example.sol
---
+++ ERC1822/UUPS_Example.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: None
+pragma solidity 0.8.0;
+
+import "https://github.com/aloycwl/Proxy.sol/blob/main/ERC1822/UUPS.sol";
+
+contract NFTExample is UUPSUpgradeable {
+
+    uint public totalSupply;
+    string public name;
+
+    function initialize(uint totalSupply_) external {
+        init();
+        (totalSupply, name) = (totalSupply_, "Crazy NFT");
+    }
+
+    function mint() external virtual {
+        require(--totalSupply > 0x01, "Ran out of supply");
+    }
+}
+
+contract NFTExampleV2 is NFTExample {
+
+    uint constant public NFTVersion = 0x02;
+
+    constructor() {
+        name = "Wild NFT";
+    }
+
+    function addSupply() external {
+        totalSupply += 0x0f;
+    }
+
+    function mint() external override {
+        require(--totalSupply > 0x00, "Ran out of supply");
+    }
+}
